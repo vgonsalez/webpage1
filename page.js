e4bb261f4b2d5081b1d57e484ddd9057a15770a0
vgonsalez <gonsalez@yahoo.com>
2019-07-12 17:10:37 -0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,19 @@
+
+            
+         
+            function link1(){
+                var write = "<i>here goes one text and it accepts html</i>";
+                document.getElementById('text').innerHTML = write;
+            }
+
+            function link2(){
+                var write = "<u>here's another text</u>";
+                document.getElementById('text').innerHTML = write;
+
+            }
+
+            function link3(){
+                var write = "<b>and a third one. Your can have as many functions you want each one one text</b>";
+                document.getElementById('text').innerHTML = write;
+            }
+        
